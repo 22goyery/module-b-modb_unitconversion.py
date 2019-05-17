998b86b65d53fc577dfc418fac07e14a8154b96b
22goyery <49483199+22goyery@users.noreply.github.com>
2019-05-17 08:34:59 -0400

    Add files via upload

---
+++ modb_unitconversion.py
@@ -0,0 +1,17 @@
+def main()
+   print("alt key to start")
+
+   print("what is the unit when converted")
+   unit =eval(input("what is it converted ?")
+   converted =5 * 5 * unit + 45
+   print( "the unit is", converted, "decimal")
+
+   # end the program
+   input("press the space key to exit.")
+
+
+   main()
+
+
+
+
